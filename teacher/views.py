d6009e3f746552e035fbe425ea464556d9f6f647
Nickolai Rovdo <nichogaus@gmail.com>
2016-11-20 14:23:04 +0300

    added table + card is ready + button to table

--- teacher/views.py
+++ teacher/views.py
@@ -2,8 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'index.html')
+
 
 def bootstrap(request):
     return render(request, 'bootstrap.html')
@@ -11,3 +14,27 @@ def bootstrap(request):
 
 def testing(request):
     return render(request, 'testing.html')
+
+
+def reviews(request):
+    return render(request, 'reviews.html')
+
+
+class Student():
+    def __init__(self, name, graduate_year, rt, ct):
+        self.name = name
+        self.graduate_year = graduate_year
+        self.rt = rt
+        self.ct = ct
+        self.rise = ct-rt
+
+
+def central_testing(request):
+    people = [
+        Student("Яна Поленкова", 2015, 56, 66),
+        Student("Костя Жигалев", 2010, 26, 77),
+        Student("Вася Пупкин", 2012, 77, 99),
+        Student("Фёдор Нагараев", 2010, 74, 76),
+        Student("Вячеслав Весёлый", 2011, 16, 66)
+    ]
+    return render(request, 'central_testing.html', {"people": people})
--- uubogdanov/urls.py
+++ uubogdanov/urls.py
@@ -5,5 +5,7 @@ from teacher import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^bootstrap/$', views.bootstrap, name='bootstrap'),
-    url(r'^testing/$', views.testing, name='testing')
+    url(r'^testing/$', views.testing, name='testing'),
+    url(r'^reviews/$', views.reviews, name='reviews'),
+    url(r'^central_testing/$', views.central_testing, name='central_testing')
 ]

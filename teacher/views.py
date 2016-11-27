6a3931ff9f87af1487f327c6f35f747a659191a0
Nickolai Rovdo <nichogaus@gmail.com>
2016-11-27 20:19:19 +0300

    reviews added

--- teacher/views.py
+++ teacher/views.py
@@ -4,8 +4,35 @@ from django.shortcuts import render
 # Create your views here.
 
 
+class Review:
+    def __init__(self, title, name, content):
+        self.title = title
+        self.name = name
+        self.content = content
+
+
+class Student:
+    def __init__(self, name, graduate_year, rt, ct):
+        self.name = name
+        self.graduate_year = graduate_year
+        self.rt = rt
+        self.ct = ct
+        self.rise = ct-rt
+
+
 def index(request):
-    return render(request, 'index.html')
+    r = [
+        Review("This is the first slide",
+               "A short description of the contents of this slide",
+               "<p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directedconvergence without revolutionary ROI.</p><p>Efficiently unleash cross-media information without cross-media value. Quickly maximize timelydeliverables for real-time schemas. Dramatically maintain clicks-and-mortar solutions withoutfunctional solutions.</p><p>Completely synergize resource taxing relationships via premier niche markets. Professionallycultivate one-to-one customer service with robust ideas. Dynamically innovate resource-levelingcustomer service for state of the art customer service.</p>"),
+        Review("This is the first slide",
+               "A short description of the contents of this slide",
+               "<p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directedconvergence without revolutionary ROI.</p><p>Efficiently unleash cross-media information without cross-media value. Quickly maximize timelydeliverables for real-time schemas. Dramatically maintain clicks-and-mortar solutions withoutfunctional solutions.</p><p>Completely synergize resource taxing relationships via premier niche markets. Professionallycultivate one-to-one customer service with robust ideas. Dynamically innovate resource-levelingcustomer service for state of the art customer service.</p>"),
+        Review("This is the first slide",
+               "A short description of the contents of this slide",
+               "<p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directedconvergence without revolutionary ROI.</p><p>Efficiently unleash cross-media information without cross-media value. Quickly maximize timelydeliverables for real-time schemas. Dramatically maintain clicks-and-mortar solutions withoutfunctional solutions.</p><p>Completely synergize resource taxing relationships via premier niche markets. Professionallycultivate one-to-one customer service with robust ideas. Dynamically innovate resource-levelingcustomer service for state of the art customer service.</p>")
+    ]
+    return render(request, 'index.html', {"reviews": r})
 
 
 def bootstrap(request):
@@ -19,14 +46,6 @@ def testing(request):
 def reviews(request):
     return render(request, 'reviews.html')
 
-
-class Student():
-    def __init__(self, name, graduate_year, rt, ct):
-        self.name = name
-        self.graduate_year = graduate_year
-        self.rt = rt
-        self.ct = ct
-        self.rise = ct-rt
 
 
 def central_testing(request):
